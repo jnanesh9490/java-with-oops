dd1b5da4704df479adb42a265550a1e9be781cab
jnanesh9490 <89685404+jnanesh9490@users.noreply.github.com>
2021-11-12 11:13:51 +0530

    Create ExceptionHandling.java

---
+++ Exception/ExceptionHandling.java
@@ -0,0 +1,33 @@
+package sample;
+import java.util.*;
+
+public class Exception {
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		int totalrunsscored;
+		int totaloversfaced;
+		float currentrunrate=0;
+		
+		System.out.println("Enter the total runs scored");
+		totalrunsscored=sc.nextInt();
+		System.out.println("Enter the total overs faced");
+		totaloversfaced=sc.nextInt();
+		try {
+			 currentrunrate=totalrunsscored/totaloversfaced;
+			
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		catch(NumberFormatException ne)
+		{
+			System.out.println(ne);
+		}
+		System.out.println("currentrunrate="+currentrunrate);
+		
+	}
+	
+
+}
